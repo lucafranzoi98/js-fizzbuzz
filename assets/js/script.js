f1449eacba08c67b3414291437258f5648b28d69
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-08-25 14:35:40 +0200

    print on page of the elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,29 @@
+const containerEl = document.getElementById("container");
+
 for (let i=1; i<=100; i++){
 
    if (i % 3 == 0 && i % 5 == 0) {
-      console.log("FizzBuzz");
+      const FizzBuzzEl = "FizzBuzz ";
+      containerEl.append(FizzBuzzEl);
    } else if (i % 3 == 0) {
-      console.log("Fizz");
+      const Fizz = "Fizz ";
+      containerEl.append(Fizz);
    } else if (i % 5 == 0){
-      console.log("Buzz");
+      const BuzzEl = "Buzz ";
+      containerEl.append(BuzzEl);
    } else {
-         console.log(i);
+      containerEl.append(i);
    }
+
+
+   // if (i % 3 == 0 && i % 5 == 0) {
+   //    console.log("FizzBuzz");
+   // } else if (i % 3 == 0) {
+   //    console.log("Fizz");
+   // } else if (i % 5 == 0){
+   //    console.log("Buzz");
+   // } else {
+   //       console.log(i);
+   // }
+
 }
